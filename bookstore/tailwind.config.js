b0943fc3bec0a8d0b9bbedcea8355413d3b44572
locnt19 <locnt19@gmail.com>
2021-01-17 01:00:18 +0700

    Update - Config extends for TailwindCSS

--- bookstore/tailwind.config.js
+++ bookstore/tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         "4xl": "2rem",
         "5xl": "2.5rem",
         "6xl": "3rem"
+      },
+      colors: {
+        salmon: "#f65d4e",
+        lightWhite: "#faf9fd",
+        lightPink: "#ffe9e9"
+      },
+      boxShadow: {
+        light:
+          "0 20px 25px -5px rgba(0, 0, 0, 0.01), 0 10px 10px -5px rgba(0, 0, 0, 0.04)"
       }
     }
   },
