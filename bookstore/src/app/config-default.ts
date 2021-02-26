7bc67e996b130a7146f6b498a6ba5f45f61b209a
locnt19 <locnt19@gmail.com>
2021-02-27 02:26:53 +0700

    Define routing map & config default

---
+++ bookstore/src/app/config-default.ts
@@ -0,0 +1,76 @@
+import { routingMaps } from "./routingMaps";
+
+export const CONFIG_DEFAULT = {
+  sidebar: [
+    {
+      id: "5fa6bf15-7089-43e3-839a-2c07a3aedff2",
+      label: "All",
+      route: routingMaps.HOME,
+      hideOnDesktop: false,
+      hideOnMobile: false
+    },
+    {
+      id: "36dd034f-34d4-4094-9fee-8979ae9974af",
+      label: "Book detail",
+      route: routingMaps.BOOK_DETAIL,
+      hideOnDesktop: false,
+      hideOnMobile: false
+    },
+    {
+      id: "16fa245f-15a9-4e7c-851d-9aeb933247d4",
+      label: "Discover",
+      route: routingMaps.DISCOVER,
+      hideOnDesktop: true,
+      hideOnMobile: false
+    },
+    {
+      id: "818396d2-7268-48f5-a366-bf155b0f10a8",
+      label: "Popular",
+      route: routingMaps.POPULAR,
+      hideOnDesktop: true,
+      hideOnMobile: false
+    }
+  ],
+  accountControls: [
+    {
+      id: "b56fd54a-d63f-4c14-83d9-7cfd328b8a0c",
+      title: "Profile",
+      route: routingMaps.HOME,
+      hideOnMobile: false,
+      hideOnDesktop: false
+    },
+    {
+      id: "ccdb5e26-6cf2-4c10-a451-f7421f6dd613",
+      title: "Notifications",
+      route: routingMaps.HOME,
+      hideOnMobile: false,
+      hideOnDesktop: true
+    },
+    {
+      id: "541f78f9-2a62-4411-9c1d-29fc693fa25f",
+      title: "Account settings",
+      route: routingMaps.HOME,
+      hideOnMobile: false,
+      hideOnDesktop: false
+    },
+    {
+      id: "4d5ee656-68b1-41eb-a080-43c504a0ec6a",
+      title: "Sign out",
+      route: routingMaps.HOME,
+      hideOnMobile: false,
+      hideOnDesktop: false
+    }
+  ],
+  dropdownSort: [
+    {
+      id: "402ce1a3-ac21-476e-97f9-ac972d8b44e5",
+      label: "Newest",
+      route: routingMaps.ROOT
+    },
+    {
+      id: "b383b145-37cf-4189-81ed-c1a19d9b2c52",
+      label: "Alphabet",
+      route: routingMaps.ROOT
+    }
+  ]
+};
---
+++ bookstore/src/app/routingMaps.ts
@@ -0,0 +1,7 @@
+export const routingMaps = {
+  ROOT: "",
+  HOME: "home",
+  BOOK_DETAIL: "book-detail",
+  DISCOVER: "discover",
+  POPULAR: "popular"
+};
