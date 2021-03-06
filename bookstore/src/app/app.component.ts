b288dd0039a056d8936c52271129b20de59593b7
locnt19 <locnt19@gmail.com>
2021-03-06 12:19:03 +0700

    Default layout one column

--- bookstore/src/app/app.component.ts
+++ bookstore/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subject } from "rxjs";
+import { NavigationEnd, Router } from "@angular/router";
+import { Observable, Subject } from "rxjs";
+import { filter, takeUntil } from "rxjs/operators";
 import { SidebarService } from "./components/sidebar/sidebar.service";
+import { routingMaps } from "./routingMaps";
 
 @Component({
   selector: "app-root",
@@ -8,11 +11,35 @@ import { SidebarService } from "./components/sidebar/sidebar.service";
   styles: []
 })
 export class AppComponent implements OnInit, OnDestroy {
+  private _navigationEnd$: Observable<NavigationEnd>;
   private _destroy$: Subject<void> = new Subject<void>();
+  private _pageDefaultLayout: Array<string>;
+  private _currentPage: string;
 
-  constructor(public sidebarService: SidebarService) {}
+  isDefaultLayout: boolean;
 
-  ngOnInit() {}
+  constructor(private _router: Router, public sidebarService: SidebarService) {
+    this._navigationEnd$ = _router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      takeUntil(this._destroy$)
+    );
+
+    this._pageDefaultLayout = [routingMaps.HOME].map(route => {
+      return "/" + route;
+    });
+  }
+
+  ngOnInit() {
+    this._navigationEnd$
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((event: NavigationEnd) => {
+        this._currentPage = event.url;
+
+        this.isDefaultLayout = this._pageDefaultLayout.includes(
+          this._currentPage
+        );
+      });
+  }
 
   ngOnDestroy() {
     this._destroy$.next();
